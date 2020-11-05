1473c75057573a4c6a20e18cb8ccd5b13f98b9a0
Per Palsboll <palsboll@gmail.com>
2020-11-05 10:04:29 +0100

    changed input to files

--- scripts/ppg-setup.sh
+++ scripts/ppg-setup.sh
@@ -14,7 +14,7 @@ echo 'Making directories'
 
 for folder in $(ls ~/popgen | egrep 'assign*|mini*|prac*'); 
 do 
-	mv ~/popgen/$folder ~/$older; 
+	mv ~/popgen/$folder ~/$folder; 
 
 done
 
@@ -24,7 +24,7 @@ echo "Copying and compiling scripts and helper programs"
 
 gcc -o ~/popgen/scripts/ms_usat_conversion ~/popgen/scripts/ms_usat_conversion.c -lm
 
-echo 'Changing permissions for scripts can be executed'
+echo 'Changing permissions for scripts to enable execution'
 
 chmod 755 ~/popgen/scripts/*
 
@@ -36,32 +36,26 @@ echo 'Modifying your .bash_profile'
 		
 echo 'export PATH=/home/$USER/popgen/scripts:$PATH' >> ~/.bash_profile
 
-#Need to load R=modules as well
-echo 'module load ms Structure R/4.0.0-foss-2020a GDAL/3.0.4-foss-2020a-Python-3.8.2' >> ~/.bash_profile
+#adding load modules to .bash_profile
 
-echo '~/popgen/scripts/load_libs.r' >> ~/.bash_profile
+echo 'module load ms Structure R/4.0.0-foss-2020a GDAL/3.0.4-foss-2020a-Python-3.8.2' >> ~/.bash_profile
 
 #R environment
 
 echo 'Setting up the R environment and installing R packages'
 
 mkdir -p ~/R/x86_64-pc-linux-gnu-library/4.0
-echo 'R_LIBS_USER=R/x86_64-pc-linux-gnu-library/4.0' >> ~/.Renviron
+mv ~/popgen/scripts/renviron ~/.Renviron
 
 source ~/.Renviron
 
-echo 'cat(".Rprofile: Setting R repository:")
-repo = getOption("repos") 
-# set up the server from which you will download the package.
-repo["CRAN"] = "https://mirror.lyrahosting.com/CRAN" 
-options(repos = repo)
-rm(repo)' >> ~/.Rprofile
+mv ~/popgen/scripts/rprofile ~/.Rprofile
 
 source ~/.Rprofile
 
 #install needed R packages
 
-~/scripts/ppg_setup_install_r_packages.r
+Rscript ~/scripts/ppg_setup_install_r_packages.r
 
 echo 'Resourcing your .bash_profile'
 
